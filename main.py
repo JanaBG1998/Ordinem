d0f4aaafce701a86cea5a6eca339c4bc9688f7e8
Fisch21 <jonasfischer43@gmx.de>
2024-03-20 08:18:06 +0100

    Daten in Datenbank einpflegen

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
+import sqlite3
 
-'''Test '''
+with sqlite3.connect('inv.db') as conn:
+    c = conn.cursor()
+
+
+    c.execute('''CREATE TABLE IF NOT EXISTS buch 
+    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    buchtitel TEXT NOT NULL)''')
+
+
+'''c.execute("INSERT INTO buch (buchtitel)VALUES ('Harry Potter')")
+
+c.execute("INSERT INTO buch (buchtitel)VALUES ('Potter Harry')")
+'''
+
+c.execute("DELETE FROM buch")
+conn.commit()
+
+
+
+
+def printDb(db):
+    c.execute("SELECT * FROM buch")
+    rows=c.fetchall()
+
+    for row in rows:
+        print(row)
+
+
+printDb('buch')
+
+c.close()
+
